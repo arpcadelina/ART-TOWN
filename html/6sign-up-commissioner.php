c9fa8cf9e9c79e97fffcbc0aecbe676bcc98be3b
John Loyd Adobas <74396545+jlcadobas@users.noreply.github.com>
2022-02-27 15:56:20 +0800

    change file path and comment tags

--- html/6sign-up-commissioner.php
+++ html/6sign-up-commissioner.php
@@ -2,7 +2,7 @@
     //Include filename for scripts/server.php to check db for info
     //Check for errors if there's a similar name and password
     //File path might change depending on the address
-    include('C:\xampp\htdocs\art-town\scripts\server.php');
+    include('C:\xampp\htdocs\ART-TOWN-main\scripts\server.php');
 ?>
 
 <!DOCTYPE html>
@@ -28,7 +28,7 @@
                 
                     //Include error.php file in case there's a similar email and full name
                     //File path might change
-                    include('C:\xampp\htdocs\art-town\scripts\errors.php');
+                    include('C:\xampp\htdocs\ART-TOWN-main\scripts\errors.php');
                 ?>
                 <label for="fullname"> Full Name</label><br><br>
                     <input type="text" id="fullname" name="username" value="<?php echo $username; ?>" required><br><br><br>
@@ -39,7 +39,7 @@
                 <label for="password"> Password </label><br><br>
                     <input type="text" id="password" name="password" value="<?php echo $password; ?>" required>
                 
-                //The anchor tag could be removed, the type="submit" is crucial for form submission 
+                <!--The anchor tag could be removed, the type="submit" is crucial for form submission -->
                 <a href="../index.html"><button class="log-in-btn" type="submit" name="reg_user"><b> Sign-Up </b></button></a>
             </form>
             <p class="account-prompt">Already have an account? <a href="5sign-in-commissioner.html"> Sign In! </a></p>

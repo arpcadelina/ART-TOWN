413e60cfc9eb91b0af12a5bb2f42d7db45f37d4c
John Loyd Adobas <74396545+jlcadobas@users.noreply.github.com>
2022-03-01 19:54:36 +0800

    currently record a log in

---
+++ scripts/session.php
@@ -0,0 +1,18 @@
+<?php
+
+    session_start();
+
+    if (!isset($_SESSION['email'])) {  
+
+        $_SESSION['msg'] = "You must log in first";
+        header('location: ./html/4sign-in-artist.php');
+    }
+
+    if (isset($_GET['logout'])) {
+
+
+        session_destroy();
+        unset($_SESSION['email']);
+        header("location: ./html/4sign-in-artist.php");
+    }
+?>

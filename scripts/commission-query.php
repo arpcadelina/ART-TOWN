4c130d5c8326700e8c45ddb78e48cb013305ecf2
John Loyd Adobas <74396545+jlcadobas@users.noreply.github.com>
2022-03-03 19:53:24 +0800

    add artworkname and upload variable

--- scripts/commission-query.php
+++ scripts/commission-query.php
@@ -1,11 +1,13 @@
 <?php
 
     //Initialize Variables
+    $artworkName   = "";
     $clientName    = "";
     $clientEmail   = "";
     $contactNumber = "";
     $address       = "";
     $details       = "";
+    $upload        = ""; 
     $errors         = array();  
 
     //Insert commission form data to db
@@ -14,24 +16,28 @@
 
     if (isset($_POST['add_commission'])) {
 
+        $artworkName   = mysqli_real_escape_string($db, $_POST['artworkName']);
         $clientName    = mysqli_real_escape_string($db, $_POST['clientName']);
         $clientEmail   = mysqli_real_escape_string($db, $_POST['clientEmail']);
         $contactNumber = mysqli_real_escape_string($db, $_POST['contactNumber']);
         $address       = mysqli_real_escape_string($db, $_POST['address']);
         $details       = mysqli_real_escape_string($db, $_POST['details']);
+        $upload        = mysqli_real_escape_string($db, $_POST['upload']);
 
             //Check if form inputs are empty
-
+            
+            if (empty($artworkName)) { array_push($errors, "Artwork Name is required"); }
             if (empty($clientName)) { array_push($errors, "Name is required"); }
             if (empty($clientEmail)) { array_push($errors, "Email is required"); }
             if (empty($contactNumber)) { array_push($errors, "Contact Number is required"); }
             if (empty($address)) { array_push($errors, "Address is required"); }
             if (empty($details)) { array_push($errors, "Additional Detail is required"); }
             
+            
                 if (count($errors) == 0) {
 
-                    $query_commission = "INSERT INTO commissions (clientname, clientemail, contactnumber, address, details) 
-                    VALUES ('$clientName', '$clientEmail', '$contactNumber', '$address', '$details')";
+                    $query_commission = "INSERT INTO commissions (artworkname, clientname, clientemail, contactnumber, address, details) 
+                    VALUES ('$artworkName', '$clientName', '$clientEmail', '$contactNumber', '$address', '$details', '$upload')";
 
                     mysqli_query($db, $query_commission);
                     $_SESSION['submit-commission'] = "Your commission was submitted.";

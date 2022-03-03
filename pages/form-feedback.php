0cf55c18529cf1ff4e9e03cb35afb75ccaaac40c
Allyza Cadeliña <72682339+mnslnln@users.noreply.github.com>
2022-03-04 03:24:20 +0800

    Update form-feedback.php

--- pages/form-feedback.php
+++ pages/form-feedback.php
@@ -18,27 +18,22 @@
         <section class="form-feedback-section">
             <div class="feedback-div">
                 <header class="ftitle"> Feedback Form </header>
-                <form class="feedback">
-		            <?php
-                        //Include error.php file in case there's a similar email and full name
-                        //File path might change
-                        include('C:\xampp\htdocs\ART-TOWN-main\scripts\errors.php');
-                    ?> 	
+                <form class="feedback" method="post" enctype="multipart/form-data" id="commission-form">
                     <label for="art-style"> Artist </label><br><br>
                         <select id="dropdown" name="artistName" value="<?php echo $artistName; ?>">
-						    <option value = "artist-1" selected>Mcleinarts</option>
-						    <option value = "artist-2">Kapebeansis</option>
-						    <option value = "artist-3">Chachiearts</option>
-						    <option value = "artist-4">Aeonix</option>
-						    <option value = "artist-5">Salmonfishuu</option>
-						    <option value = "artist-6">Art of Ran</option>
-						    <option value = "artist-7">Yurieem</option>
+						    <option value = "Mcleinarts" selected>Mcleinarts</option>
+						    <option value = "Kapebeansis">Kapebeansis</option>
+						    <option value = "Chachiearts">Chachiearts</option>
+						    <option value = "Aeonix">Aeonix</option>
+						    <option value = "Salmonfishuu">Salmonfishuu</option>
+						    <option value = "Art of Ran">Art of Ran</option>
+						    <option value = "Yurieem">Yurieem</option>
 					    </select><br><br>
                     <label for="name"> Name </label><br><br>
                         <input type="text" id="name" name="commissionerName" value="<?php echo $commissionerName; ?>"/><br><br>
 
                     <label for="rate"> How did your artist do? Rate the art. </label><br><br>
-                        <select>
+                        <select id="dropdown" name="ratingNum" value="<?php echo $ratingNum; ?>">
 						    <option value = "5" selected>5 ✮✮✮✮✮</option>
                             <option value = "4">4 ✮✮✮✮</option>
                             <option value = "3">3 ✮✮✮</option>
@@ -47,7 +42,7 @@
                         </select><br><br>
                     <label for="feedback-details"> Please share your feedback: </label><br><br>
                         <textarea name="feedbackDetails" value="<?php echo $feedbackDetails; ?>"> </textarea><br><br>
-		            <a href="3commissionlist.php"><button class="long-btn"><b> Submit </b></button></a>
+		            <a href="3commissionlist.php"><button class="long-btn" id="feedback-form" name = 'add_feedback' type="submit"><b> Submit </b></button></a>
                 </form>
             </div>
             <div class="feedback-img">

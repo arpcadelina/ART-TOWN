0b90dc5e4044594b50352f1edbed8d916db32a7e
Allyza Cadeliña <72682339+mnslnln@users.noreply.github.com>
2022-03-04 03:23:58 +0800

    Update feedback_query.php

--- scripts/feedback_query.php
+++ scripts/feedback_query.php
@@ -5,6 +5,7 @@
     $artistName       = "";
     $commissionerName = "";
     $feedbackDetails  = "";
+    $ratingNum        = "";
     $errors         = array();
 
     //Connect to database
@@ -16,21 +17,24 @@
         $artistName         = mysqli_real_escape_string($db, $_POST['artistName']);
         $commissionerName   = mysqli_real_escape_string($db, $_POST['commissionerName']);
         $feedbackDetails    = mysqli_real_escape_string($db, $_POST['feedbackDetails']);
+        $ratingNum          = mysqli_real_escape_string($db, $_POST['ratingNum']);
 
         if (empty($artistName)) { array_push($errors, "Artist Name is required"); }
         if (empty($commissionerName)) { array_push($errors, "Commissioner Name is required"); }
         if (empty($feedbackDetails)) { array_push($errors, "Feedback Details is required"); }
+        if (empty($ratingNum)) { array_push($errors, "Rating is required"); }
+
 
             
-            if (count($errors) == 0) {
+            if (count($errors) === 0) {
 
-                $query_feedback = "INSERT INTO feedbacks (artistname, commissionername, feedback) 
-                VALUES ('$artistName', '$commissionerName', '$feedbackDetails')";
+                $query_feedback = "INSERT INTO feedbacks (artistname, commissionername, rating, feedback) 
+                VALUES ('$artistName', '$commissionerName', '$ratingNum', '$feedbackDetails')";
 
                 mysqli_query($db, $query_feedback);
                 $_SESSION['submit-feedback'] = "Your feedback was submitted.";
                 $form_submit_message = $_SESSION['submit-feedback'];
-                header('location: ../index.php');
+                header('location: 3commissionlist.php');
             }
     }
 ?>

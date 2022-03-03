88efe00209583012686df011e933905de42889c5
John Loyd Adobas <74396545+jlcadobas@users.noreply.github.com>
2022-03-03 13:43:41 +0800

    change link on log out

--- scripts/session.php
+++ scripts/session.php
@@ -2,17 +2,19 @@
 
     session_start();
 
+/*
     if (!isset($_SESSION['email'])) {  
 
         $_SESSION['msg'] = "You must log in first";
         header('location: ./html/4sign-in-artist.php');
     }
+*/
 
     if (isset($_GET['logout'])) {
 
 
         session_destroy();
         unset($_SESSION['email']);
-        header("location: ./html/4sign-in-artist.php");
+        header("location: index.php");
     }
 ?>
